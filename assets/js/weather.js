a7ead4104a5791c519ee5e90a7a1ff12f2b271c5
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-03-01 17:52:19 -0700

    current city, current weather display functional

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -5,6 +5,8 @@ var windSpeed = document.getElementById('windSpeed');
 var humidity = document.getElementById('humidity');
 var uvIndex = document.getElementById('uvIndex');
 
+var cities = [];
+
 searchBtn.addEventListener('click', function(e){
     e.preventDefault()
     var inputValue = document.getElementById('inputValue').value;
@@ -13,9 +15,25 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&units=i
         .then(data => {
             var date = moment().format("MMMM Do YYYY, h:mm a");
             displayDate.innerText= inputValue + " " + "(" + date + ")";
+            img.setAttribute("src", `https://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`);;
             var tempValue = data['main']['temp'] + " degrees";
             temp.innerText = "Temp: " + tempValue;
             windSpeed.innerText = "Wind: " + data['wind']['speed']  + " MPH";
             humidity.innerText = "Humidity: " + data['main']['humidity'] + "%";
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            displayUvIndex(lat, lon);
         })
     });
+
+
+    function displayUvIndex (lat, lon) {
+        fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&APPID=f9d491be2c04cabd7a30f6d0ce1b0f1e")
+        .then(response => response.json())
+        .then(data => {
+            uvIndex.innerText = 'UV Index: ' + data['current']['uvi'];
+            
+        }).
+        catch(err => alert('Error' + err))
+    
+    }
