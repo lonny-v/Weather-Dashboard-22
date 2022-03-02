2371f7f22acfb6b6039f1ad752309bdbe19109fc
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-03-01 17:23:52 -0700

    display city and date/time functionality

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+var searchBtn = document.getElementById('searchBtn');
+var displayDate= document.getElementById('displayDate');
+var temp = document.getElementById('temp');
+var windSpeed = document.getElementById('windSpeed');
+var humidity = document.getElementById('humidity');
+var uvIndex = document.getElementById('uvIndex');
+
+searchBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    var inputValue = document.getElementById('inputValue').value;
+fetch ('https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&units=imperial&APPID=f9d491be2c04cabd7a30f6d0ce1b0f1e')
+        .then(response => response.json())
+        .then(data => {
+            var date = moment().format("MMMM Do YYYY, h:mm a");
+            displayDate.innerText= inputValue + " " + "(" + date + ")";
+        })
+        console.log(inputValue);
+    });
